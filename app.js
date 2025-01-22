536e53cf0c22347d6e376ad14768b8c6b9099ee3
Grand-Merlin <mignon.francois@gmail.com>
2025-01-22 18:20:32 +0100

    implementation des recherche

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const db = new pg.Client({
     port: 5432,
 });
 db.connect();
+let factures = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -24,15 +25,29 @@ app.get("/", async(req, res) => {
     const facturesByNum = resultByNum.rows;
     res.render("index.ejs", {
         facturesByDate,
-        facturesByNum
+        facturesByNum,
+        factures
     });
 });
 
 app.post("/search", async(req, res)=>{
-    console.log(req.body.searchField);
     console.log(req.body.searchSelect);
+    console.log(req.body.searchField);
+    console.log(req.body.searchDate);
+    let param = "";
+    if (req.body.searchDate) {
+        param = req.body.searchDate
+    } else{
+        param = req.body.searchField
+    }
+    const result = await db.query(`SELECT * FROM invoice AS i FULL OUTER JOIN customers AS c ON i.cust_id = c.id WHERE ${req.body.searchSelect} = $1 ORDER BY inv_number ASC`, [param]);
+    factures = [];
+    result.rows.forEach(facture => {
+        factures.push(facture);
+    });
+    res.redirect("/");
 });
 
 app.listen(port, () => {
-    console.log(`Le serveur a démarré sur http://localhost:${port}`);
+    console.log(`Le serveur a démarré sur http://127.0.0.1:${port}`);
 });
