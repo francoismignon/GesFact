79576997f8d0d019616b144a8d4a1ce6d829427d
Grand-Merlin <mignon.francois@gmail.com>
2025-01-27 16:41:45 +0100

    fonction des calcule des differente TVA et montant

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
-import Facture from "./models/Facture.js"
-import Article from "./models/Article.js";
-import FactureDetail from "./models/FactureDetail.js";
 
 
 const app = express();
@@ -21,9 +18,23 @@ let lastInvNum;
 let clients = [];
 let items = [];
 let details =[];
+let base21 = 0;
+let base6 = 0;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+function computeUnitHtva(montant, quantite){
+    return montant * quantite;
+}
+
+function computeUnitTva21(prixHtva, tva){
+   return prixHtva * (1 + (parseInt(tva)/100)); 
+}
+
+function computeUnitTva6(prixHtva, tva){
+    return prixHtva * (1 + (parseInt(tva)/100));
+}
 
 app.get("/", async (req, res) => {
     const resultByDate = await db.query("SELECT * FROM invoice ORDER BY inv_date ASC");
@@ -115,20 +126,52 @@ app.post("/searcharticle", async(req, res)=>{
 
 app.post("/addtodetails", async(req, res)=>{
     const id = req.body.id;
-    const result = await db.query("SELECT * FROM item AS i INNER JOIN vattype AS vt ON i.vat_type_id = vt.id INNER JOIN vat AS v ON v.vat_type_id = vt.id WHERE i.id = $1", [id]);
-    console.log(result.rows);
+    const result = await db.query("SELECT i.id, i.item_label, i.item_description, i.item_retail_price, v.vat_percentage  FROM item AS i INNER JOIN vattype AS vt ON i.vat_type_id = vt.id INNER JOIN vat AS v ON v.vat_type_id = vt.id WHERE i.id = $1", [id]);
     result.rows.forEach(itemVat => {
         details.push(itemVat);
     });
     res.render("facture.ejs", {
         clients,
-        details
+        details,
     });
 
 });
 
 app.post("/validligne", async(req, res)=>{
-    
+    let factureDetails = [];
+    details.forEach((detail, index) => {
+        let d = {};
+        d.id = detail.id;
+        d.line_number = index + 1;
+        d.item_label = detail.item_label;
+        d.item_description = detail.item_description;
+        d.item_retail_price = detail.item_retail_price;
+        d.vat_percentage = detail.vat_percentage;
+        d.qty = req.body["inputQty" + detail.id];
+        d.discount = req.body["inputOpt" + detail.id];
+        factureDetails.push(d);
+    });
+    factureDetails.forEach(d => {
+        d.totHtva = computeUnitHtva(d.item_retail_price, d.qty);
+    });
+    factureDetails.forEach(d => {
+        console.log(typeof(d.totHtva));
+        console.log(d.totHtva);
+        if (d.vat_percentage === "21.00") {
+            let totTva = computeUnitTva21(d.totHtva, d.vat_percentage);
+            base21 += totTva;
+        } else {
+            let totTva = computeUnitTva6(d.totHtva, d.vat_percentage);
+            base6 += totTva ;
+        }
+    });
+
+    res.render("facture.ejs", {
+        clients,
+        factureDetails,
+        base21,
+        base6
+    });
 });
 
 app.listen(port, () => {
